7f6cab84a2720c6b7a2b122d4e3abec00bf4344f
Sharla Gelfand <sharla.gelfand@gmail.com>
2022-08-24 09:01:32 -0400

    Palette style in striped

--- R/build_striped.R
+++ R/build_striped.R
@@ -1,24 +1,54 @@
 #' @export
 build_striped <- function(data, palette, palette_style) {
-
-  df <- dplyr::tibble(
+  rect <- dplyr::tibble(
     xmin = seq(data$xmin, data$xmax, length.out = sample(20:50, 1)),
     xmax = dplyr::lead(xmin)
   ) %>%
     dplyr::mutate(
       xmax = dplyr::coalesce(xmax, data$xmax),
-      fill = ifelse(dplyr::row_number() %% 2 == 0, colours[["lightpink"]], colours[["darkpink"]]),
       geom = "rect",
       ymin = data$ymin,
       ymax = data$ymax
     )
 
-  if (sample(c(TRUE, FALSE), 1)) {
-    df <- df %>%
+  if (palette_style == "mono") {
+    rect <- rect %>%
       dplyr::mutate(
-        fill = ifelse(!dplyr::row_number() %% 2 == 0, colours[["lightpink"]], colours[["darkpink"]])
+        fill = ifelse(dplyr::row_number() %% 2 == 0, palette, "black")
       )
+  } else if (palette_style == "duo") {
+    style <- sample(c("alternating", "blocks"), 1)
+
+    if (true_or_false()) {
+      palette <- rev(palette)
+    }
+
+    if (style == "alternating") {
+      rect <- rect %>%
+        dplyr::mutate(
+          fill = ifelse(dplyr::row_number() %% 2 == 0, palette[[1]], palette[[2]])
+        )
+    } else if (style == "blocks") {
+      n_blocks <- sample(2:10, 1)
+      block_starts <- sample(1:nrow(rect), n_blocks) %>%
+        sort()
+      block_starts <- dplyr::tibble(id = block_starts) %>%
+        dplyr::mutate(fill = ifelse(dplyr::row_number() %% 2 == 0, palette[[1]], palette[[2]]))
+
+      rect <- rect %>%
+        dplyr::mutate(id = dplyr::row_number()) %>%
+        dplyr::left_join(block_starts, by = "id") %>%
+        tidyr::fill(fill, .direction = "downup")
+
+      if (true_or_false()) {
+        rect <- rect %>%
+          dplyr::mutate(fill = ifelse(id %% 2 == 0, fill, "black"))
+      } else {
+        rect <- rect %>%
+          dplyr::mutate(fill = ifelse(id %% 2 != 0, fill, "black"))
+      }
+    }
   }
 
-  df
+  rect
 }
